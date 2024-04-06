39204b554cdf37cdefef849931735c1a6ff07a15
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-04-06 18:45:30 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import geometry.*;
+
+public class zad6 {
+    public static void main(String[] args) {
+        Square square = new Square(5);
+
+        System.out.println("Width of the square: " + square.toString());
+        System.out.println("Width of the square: " + square.getWidth());
+    }
+}

c601784664c0fc2926e4c583c1f45ae9ba50184f
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-04-06 18:14:34 +0200

    Add files via upload

---
+++ geometry/Square.java
@@ -0,0 +1,18 @@
+package geometry;
+
+public class Square extends Rectangle {
+    public Square(double width){
+        super(width);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                '}';
+    }
+
+    public double getWidth(){
+        return width;
+    }
+}

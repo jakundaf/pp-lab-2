e7f30b51ae1a8cc72a151d52a57731d4edb01793
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-04-06 19:18:42 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class zad1 {
     public static void main(String[] args) {
         Point point = new Point(2,5);
-        System.out.println("geometry.Point's X value is: " + point.getX());
-        System.out.println("geometry.Point's Y value is: " + point.getY());
+        System.out.println("Point's X value is: " + point.getX());
+        System.out.println("Point's Y value is: " + point.getY());
     }
 }

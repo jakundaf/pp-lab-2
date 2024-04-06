6b2ec734594f3ada5c155111cc334e227bf1b460
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-04-06 19:16:51 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
-import geometry.*;
-
 public class zad1 {
     public static void main(String[] args) {
         Point point = new Point(2,5);
         System.out.println("geometry.Point's X value is: " + point.getX());
         System.out.println("geometry.Point's Y value is: " + point.getY());
-
-        Square square = new Square(3);
     }
 }

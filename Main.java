c3d29019fed0b3175333e0542561f25552ee1175
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-04-06 19:32:14 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class zad1 {
+public class Main {
     public static void main(String[] args) {
         Point point = new Point(2,5);
         System.out.println("Point's X value is: " + point.getX());

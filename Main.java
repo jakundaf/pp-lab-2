b458d7b6c6afbdcf92a71bf6ef253def5d350733
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-04-06 18:44:57 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import geometry.*;
+
+public class zad5 {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5,2);
+
+        System.out.println("Area of the rectangle is: " + rectangle.calculateArea());
+        System.out.println("Perimeter of the rectangle is: " + rectangle.calculatePerimeter());
+
+    }
+}

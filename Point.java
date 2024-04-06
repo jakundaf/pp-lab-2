131175ae4f4d698a73ce70349a1484892c72eb05
jakundaf <161923225+jakundaf@users.noreply.github.com>
2024-04-06 19:03:20 +0200

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,31 @@
+public class Point {
+    private double x;
+    private double y;
+    public Point() {
+    }
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
